c9184092e97e47b11b28f836f9b0784022e59356
Sefinek <92880265+sefinek24@users.noreply.github.com>
2022-05-07 05:17:50 +0200

    v1.0.0

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = str => (/discord\.(?:com\/invite|gg)/gim).test(str);
+module.exports = str => {
+	if (!str) throw new Error('no data was given in the function');
+
+	return (/discord(?:app\.com\/invite|\.(?:com\/invite|gg))/gim).test(str);
+};
