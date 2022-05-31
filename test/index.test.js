a730519f8f8a08c04892a37a96c120d2bc1e802d
Sefinek <92880265+sefinek24@users.noreply.github.com>
2022-05-31 10:36:26 +0200

    Update test file

--- test/index.test.js
+++ test/index.test.js
@@ -4,27 +4,26 @@ const validUrl = [
 	'https://discord.gg/example',
 	'discord.gg/example',
 	'https://diSCOrd.gg/7548215',
-	'http://discord.gg/5213567',
+	'http://discord.gg/JpAGQkhV',
 	'https://discord.com/invite/6246848',
 	'https://discordapp.com/invite/9567935',
+	'www.discord.gg/9567935',
 
 	// Disboard.org
 	'https://disboard.org/server/join/000000000000000000',
 	'https://disboard.org/en/server/join/000000000000000000',
 	'https://disboard.org/pl/server/join/000000000000000000',
 
-	// Discord.me
+	// Other links
 	'https://discord.me/server/join/direct/example',
-
-	// discordhome.com
 	'https://discordhome.com/join/00000',
 ];
 
 const randomUrl = [
 	'https://example.com',
-	'https://google.com',
-	'https://github.com/sefinek24/is-discord-invite',
+	'https://sefinek.fun',
 	'https://www.npmjs.com',
+	'https://github.com/sefinek24/is-discord-invite',
 ];
 
 test('Check for valid Discord invites', () => validUrl.forEach(url => expect(isInvite(url)).toBe(true)));
