101322ddb8bb1d711c3ed14552fce1128eb7dbe7
Sefinek <sefinek5916@gmail.com>
2023-09-07 01:17:11 +0200

    Add new function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const regex = require('./regex.js');
+const online = require('./online.js');
 
 module.exports = {
 	regex,
+	online,
 };
---
+++ test/online/index.example.js
@@ -0,0 +1,7 @@
+const isInvitation = require('../../index.js');
+
+(async () => {
+	const result = await isInvitation.online('sdfafsda  fsdasdf fsda afdshttps://discord.gg/RW32Xpdzn https://discord.gg/RW32Xpzn');
+
+	console.log(result);
+})();
