a87cda620d5ead9b8221b2c3c5d5fe45718dc6ee
Sefinek <sefinek5916@gmail.com>
2023-09-06 22:49:56 +0200

    Update tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,34 +1,37 @@
 const isInvite = require('../');
 
-const validUrl = [
-	'https://discord.gg/example',
-	'discord.gg/example',
-	'https://diSCOrd.gg/VqdtyNjS',
-	'http://discord.gg/DzRYkCR4',
-	'https://discord.com/invite/6246848',
-	'https://discordapp.com/invite/nbnhkaDL',
-	'www.discord.gg/yQYb6WYS',
-	'https://watchanimeattheoffice.com/invite/Df6KJWvU',
-	'https://discordsays.com/invite/TvaKnzsxC6',
+describe('Discord Invite Link Validation', () => {
+	const validInviteUrls = [
+		'https://discord.gg/example',
+		'discord.gg/example',
+		'https://diSCOrd.gg/VqdtyNjS',
+		'http://discord.gg/DzRYkCR4',
+		'https://discord.com/invite/6246848',
+		'https://discordapp.com/invite/nbnhkaDL',
+		'www.discord.gg/yQYb6WYS',
+		'https://watchanimeattheoffice.com/invite/Df6KJWvU',
+		'https://discordsays.com/invite/TvaKnzsxC6',
+		'https://disboard.org/server/join/000000000000000000',
+		'https://disboard.org/en/server/join/000000000000000000',
+		'https://disboard.org/pl/server/join/000000000000000000',
+		'some text 1 2 3 https://disboard.org/pl/server/join/000000000000000000 3213 1 some text',
+		'https://discord.me/server/join/direct/example',
+		'https://discordhome.com/join/00000',
+	];
 
-	// Disboard.org
-	'https://disboard.org/server/join/000000000000000000',
-	'https://disboard.org/en/server/join/000000000000000000',
-	'https://disboard.org/pl/server/join/000000000000000000',
-	'some text 1 2 3 https://disboard.org/pl/server/join/000000000000000000 3213 1 some text',
+	const invalidUrls = [
+		'https://example.com',
+		'https://discord.com',
+		'https://sefinek.net',
+		'https://www.npmjs.com',
+		'https://github.com/sefinek24/is-discord-invite',
+	];
 
-	// Other links
-	'https://discord.me/server/join/direct/example',
-	'https://discordhome.com/join/00000',
-];
+	test.each(validInviteUrls)('Valid invite link: %s', (url) => {
+		expect(isInvite(url)).toBe(true);
+	});
 
-const randomUrl = [
-	'https://example.com',
-	'https://discord.com',
-	'https://sefinek.net',
-	'https://www.npmjs.com',
-	'https://github.com/sefinek24/is-discord-invite',
-];
-
-test('Check for valid Discord invites', () => validUrl.forEach(url => expect(isInvite(url)).toBe(true)));
-test('Check for random invalid links', () => randomUrl.forEach(url => expect(isInvite(url)).toBe(false)));
+	test.each(invalidUrls)('Invalid link: %s', (url) => {
+		expect(isInvite(url)).toBe(false);
+	});
+});
