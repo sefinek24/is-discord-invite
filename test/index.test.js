5910967416b0a5fc7f056bb92de0f2213e255b64
Sefinek <sefinek5916@gmail.com>
2023-09-06 23:52:10 +0200

    Update tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,37 +1,97 @@
-const isInvite = require('../index.js');
+const isDiscordInviteLink = require('../index.js');
 
-describe('Discord Invite Link Validation', () => {
-	const validInviteUrls = [
-		'https://discord.gg/example',
-		'discord.gg/example',
-		'https://diSCOrd.gg/VqdtyNjS',
-		'http://discord.gg/DzRYkCR4',
-		'https://discord.com/invite/6246848',
-		'https://discordapp.com/invite/nbnhkaDL',
-		'www.discord.gg/yQYb6WYS',
-		'https://watchanimeattheoffice.com/invite/Df6KJWvU',
-		'https://discordsays.com/invite/TvaKnzsxC6',
-		'https://disboard.org/server/join/000000000000000000',
-		'https://disboard.org/en/server/join/000000000000000000',
-		'https://disboard.org/pl/server/join/000000000000000000',
-		'some text 1 2 3 <cat> https://disboard.org/pl/server/join/000000000000000000 3213 666 some text',
-		'https://discord.me/server/join/direct/example',
-		'https://discordhome.com/join/00000',
-	];
+describe('#isDiscordInviteLink', () => {
+	// https://discord.gg
+	describe('https://discord.gg', () => {
+		test('Valid Discord.gg link 1', () => {
+			const url = 'https://discord.gg/b8nzKetf';
+			expect(isDiscordInviteLink(url)).toBe(true);
+		});
 
-	const invalidUrls = [
-		'https://example.com',
-		'https://discord.com',
-		'https://sefinek.net',
-		'https://www.npmjs.com',
-		'https://github.com/sefinek24/is-discord-invite',
-	];
+		test('Valid Discord.gg link 2', () => {
+			const url = 'https://discord.gg/4EBN3vJ';
+			expect(isDiscordInviteLink(url)).toBe(true);
+		});
 
-	test.each(validInviteUrls)('Valid invite link: %s', url => {
-		expect(isInvite(url)).toBe(true);
+		test('Valid Discord.gg link 3', () => {
+			const url = 'foo exampple 123123 https://discord.gg/WNdbZDJm computer cat UwU :DDDDDD';
+			expect(isDiscordInviteLink(url)).toBe(true);
+		});
+
+		test('Valid link with options', () => {
+			const url = 'https://discord.gg/d9XxuDc';
+			const options = {
+				defaultDiscordUrls: true,
+			};
+			expect(isDiscordInviteLink(url, options)).toBe(true);
+		});
 	});
 
-	test.each(invalidUrls)('Invalid link: %s', url => {
-		expect(isInvite(url)).toBe(false);
+	// https://discord.com/invite
+	describe('https://discord.com/invite', () => {
+		test('Valid Discord.com/invite link 1', () => {
+			const url = 'https://discord.com/invite/b8nzKetf';
+			expect(isDiscordInviteLink(url)).toBe(true);
+		});
+
+		test('Valid Discord.gg/invite link 2', () => {
+			const url = 'https://discord.gg/invite/t6uYRKR';
+			expect(isDiscordInviteLink(url)).toBe(true);
+		});
+
+		test('Valid Discord.gg/invite link 3', () => {
+			const url = 'ABCDE123>&^*%mn$v%&$^n#^$%#hb$%^h#b<https://discord.gg/invite/t6uYRKR?qwpe{r:lqfeQWECFRQ';
+			expect(isDiscordInviteLink(url)).toBe(true);
+		});
 	});
+
+
+	// otherDiscordUrls
+	describe('otherDiscordUrls', () => {
+		test('Valid other Discord domain - watchanimeattheoffice.com', () => {
+			const url = 'https://watchanimeattheoffice.com/invite/VFbpeG48c';
+			expect(isDiscordInviteLink(url)).toBe(true);
+		});
+
+		test('Valid other Discord domain - discordsays.com', () => {
+			const url = 'https://discordsays.com/invite/TvaKnzsxC6';
+			expect(isDiscordInviteLink(url)).toBe(true);
+		});
+	});
+
+
+	// disboard
+	describe('https://disboard.org', () => {
+		test('Valid Disboard.org link', () => {
+			const url = 'https://disboard.org/en/server/join/1149087970587062392';
+			expect(isDiscordInviteLink(url)).toBe(true);
+		});
+	});
+
+	// discordMe
+	describe('https://discord.me', () => {
+		test('Valid Discord.me link', () => {
+			const url = 'https://discord.me/server/join/1149087970587062392';
+			expect(isDiscordInviteLink(url)).toBe(true);
+		});
+
+		test('Valid link with everything option', () => {
+			const url = 'https://discord.me/server/join/1149087970587062392';
+			const options = {
+				everything: true,
+			};
+			expect(isDiscordInviteLink(url, options)).toBe(true);
+		});
+	});
+
+	// discordhome
+	test('https://discordhome.com', () => {
+		const url = 'https://discordhome.com/join/1149087970587062392';
+		expect(isDiscordInviteLink(url)).toBe(true);
+	});
+});
+
+
+describe('#isNotVaildDiscordInviteLink', () => {
+
 });
