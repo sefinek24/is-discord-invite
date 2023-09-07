9c605eeb4343608de9a39f32c29f09dd8957a1e6
Sefinek <sefinek5916@gmail.com>
2023-09-07 02:34:06 +0200

    Update

--- test/online/index.example.js
+++ test/online/index.example.js
@@ -5,5 +5,5 @@ const isInvitation = require('../../index.js');
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, https://discord.gg/fgasfasd sed do eiusmod tempor incididunt ut labore https://discord.gg/000000000 et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitationhttps://discord.gg/yrtyretbhe ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velithttps://disdsgasgaesse cillum dolore eu fugiat nulla pariatur. Excepteurhttps://discord.gg/prEpvx2SRz sint occaecat cupidatat nondiscord.gg/fgasfasdproident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 	);
 
-	console.log(result); // Use result.isInvitation to check if the text contains a valid Discord invitation link
+	console.log(result); // Use `result.isInvitation` to check if the text contains a valid Discord invitation link.
 })();
