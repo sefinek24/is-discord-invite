00f4a287def6a7ff88c660c3a814128c4c912112
Sefinek <sefinek5916@gmail.com>
2023-09-07 02:28:10 +0200

    Update

--- test/online/index.test.js
+++ test/online/index.test.js
@@ -36,4 +36,43 @@ describe('#isInvitation.online', () => {
 		expect(result.url).toBe(text);
 		expect(result.invitationCode).toBe(null);
 	});
+
+	it('No valid links', async () => {
+		const text = 'This is a random text without any Discord links.';
+		const result = await isInvitation.online(text);
+
+		expect(result.success).toBe(true);
+		expect(result.code).toBe(undefined);
+		expect(result.isInvitation).toBe(false);
+		expect(result.message).toBe('Not found any Discord invitation links');
+		expect(result.url).toBe(null);
+		expect(result.invitationCode).toBe(null);
+		expect(result.discordResponse).toBe(undefined);
+	});
+
+	it('Error sending requests to the API server', async () => {
+		const text = 'https://discord.gg/invalidlink';
+		const result = await isInvitation.online(text);
+
+		expect(result.success).toBe(true);
+		expect(result.code).toBe(null);
+		expect(result.isInvitation).toBe(false);
+		expect(result.message).toBe('No valid links found');
+		expect(result.url).toBe(text);
+		expect(result.invitationCode).toBe(null);
+		expect(result.discordResponse).toBe(null);
+	});
+
+	it('zero-width space', async () => {
+		const text = '​​​​​​';
+		const result = await isInvitation.online(text);
+
+		expect(result.success).toBe(true);
+		expect(result.code).toBe(undefined);
+		expect(result.isInvitation).toBe(false);
+		expect(result.message).toBe('Not found any Discord invitation links');
+		expect(result.url).toBe(null);
+		expect(result.invitationCode).toBe(null);
+		expect(result.discordResponse).toBe(undefined);
+	});
 });
